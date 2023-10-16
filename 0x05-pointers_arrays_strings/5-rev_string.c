5678ed42346e2c068ec5573d3a70c90fa2faabb5
Mostafa Naguib <moustafanaguib200@gmail.com>
2023-10-16 12:25:54 +0300

    task five

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,18 +6,8 @@
  * Description: A function that reverses a string.
  * @s: Takes the string that you want to change it reversibly...
  *
- * Return: Always 0 (Success)
  */
 
-int main(void)
-{
-    char s[10] = "My School";
-
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
-    return (0);
-}
 
 void rev_string(char *s)
 {
