e02895a5262a49b656f659337651b0299a3f8ffd
Mostafa Naguib <moustafanaguib200@gmail.com>
2023-10-19 10:09:28 +0300

    task one two

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+char *_strcat(char *dest, char *src);
+#include <stdio.h>
+#include <string.h>
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+#include <stdlib.h>
+#include <ctype.h>
+char *string_toupper(char *str);
+char *cap_string(char *);
+char *leet(char *);
+
