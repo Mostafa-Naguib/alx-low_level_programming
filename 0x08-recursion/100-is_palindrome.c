1eb4832ed5fdf5e56a5c1206c7c539c2398bad7c
mostafa <mostafanaguib200@gmail.com>
2023-10-27 04:34:14 +0200

    palindrome task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+int helper_palindrome(char *s, int start, int len);
+
+
 /**
  * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
  *
@@ -9,18 +12,37 @@
  * Return: 0 (is palindrome) 1 (is not palindrome)
  */
 
+
 int is_palindrome(char *s)
 {
 	int len = strlen(s);
-	int i;
 
-	for (i = 0; i < len / 2; i++)
+	return (helper_palindrome(s, 0, len));
+}
+
+/**
+ * helper_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ *
+ * Description: returns 1 if a string is a palindrome and 0 if not.
+ * @s: The string
+ * @start: The starting point
+ * @len: The length of the string
+ *
+ * Return: 1 (is plindrome) 0 (is not palindrome)
+ */
+
+int helper_palindrome(char *s, int start, int len)
+{
+	if (start >= (len / 2))
 	{
-		if (s[i] != s[len - i - 1])
-		{
-			return (0);
-		}
+		return (1);
 	}
-	return (1);
+
+	if (s[start] != s[len - start - 1])
+	{
+		return (0);
+	}
+
+	return (helper_palindrome(s, start + 1, len));
 }
 
