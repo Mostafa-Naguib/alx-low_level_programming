2116eeb58972802f4075ed20d1ff301860d9bedf
mostafa <mostafanaguib200@gmail.com>
2023-10-29 10:06:11 +0200

    static library project

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert a string to an integer.
+ *
+ * Description: Convert a string to an integer.
+ * @s: The string
+ *
+ * Return: The number
+ */
+
+int _atoi(char *s)
+{
+	return (atoi(s));
+}
---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for file in *.c
+do
+	gcc -c "$file" -o "${file%.c}.o"
+done
+
+
+
+for file in *.o
+do
+	ar rc libmy.a "$file"
+done
