ffd38ed4b456fb37c07f248a93c2927669e0efc3
mostafa <mostafanaguib200@gmail.com>
2023-11-01 19:11:06 +0200

    task 4

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,13 +8,10 @@
  * @argc: The number of argv
  * @argv: The array
  *
- * Return: Always 0 (Success)
  */
 
 
-int main(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - argc]);
-
-	return (0);
 }
