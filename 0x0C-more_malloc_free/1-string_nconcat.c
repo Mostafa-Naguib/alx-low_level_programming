f09fe5c521db6575437f06c7d0c0c773c8a786e1
mostafa <mostafanaguib200@gmail.com>
2023-11-02 11:51:15 +0200

    testing

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * string_nconcat - A function that concatenates two strings.
+ *
+ * Description: A function that concatenates two strings.
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The number of byte that you need to take from "s2"
+ *
+ *
+ * Return: The string
+ */
+
+
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
+	int i = 0;
+	char *str;
+	int memory;
+	int len1 = strlen(s1);
+	unsigned int len2 = strlen(s2);
+
+	if (s1 == 0 || s2 == 0)
+	{
+		return (0);
+	}
+
+	
+	if (n > len2)
+		memory = len1 + len2;
+	else
+		memory = len1 + n;
+
+	str = malloc(memory + 1);
+	if (str == 0)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < memory; i++)
+	{
+		if (i < len1)
+		{
+			str[i] = s1[i];
+		}
+		else
+		{
+			str[i] = s2[i - len1];
+		}
+	}
+
+	str[memory + 1] = '\0';
+
+	return (str);
+
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+
+/**
+ * _calloc - a function that allocates memory for an array, using malloc...
+ *
+ * Description: a function that allocates memory for an array, using malloc...
+ * @size: How many bytes do you want to allocate in memory...
+ * @nmemb: the size of it
+ *
+ * Return: A pointer to the allocated memory.
+ */
+
+
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == 0)
+		return (0);
+
+	memset(ptr, 0, size);
+
+	return (ptr);
+}
