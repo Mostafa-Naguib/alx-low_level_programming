b315659a865b8e84a02a6e69ed9171aa20b3e0b7
mostafa <mostafanaguib200@gmail.com>
2023-11-03 09:36:31 +0200

    task one

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,24 @@
  * Return: The string
  */
 
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *concat;
+
+    concat = string_nconcat("Best ", "School !!!", 6);
+    printf("%s\n", concat);
+    free(concat);
+    return (0);
+}
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -22,11 +39,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int memory;
 	int len1 = strlen(s1);
 	unsigned int len2 = strlen(s2);
-
-	if (s1 == 0 || s2 == 0)
-	{
-		return (0);
-	}
 
 	
 	if (n > len2)
