6b0f00d97485288a48ffd46b2935a78fa78b6d8d
mostafa <mostafanaguib200@gmail.com>
2023-11-17 10:42:43 +0200

    testing

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,23 +11,5 @@
 
 int _atoi(char *s)
 {
-	int result = 0;
-	int sign = 1;
-
-	while (*s == ' ' || *s == '\t' || *s == '\n')
-		s++;
-
-	if (*s == '-' || *s == '+')
-	{
-		sign = (*s == '-') ? -1 : 1;
-		s++;
-	}
-
-	while (*s >= '0' && *s <= '9')
-	{
-		result = result * 10 + (*s - '0');
-		s++;
-	}
-
-	return (result * sign);
+	return atoi(s);
 }
