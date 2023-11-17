886e8c8fac81c8ce897047d37c43fe04773c80ed
mostafa <mostafanaguib200@gmail.com>
2023-11-17 16:56:44 +0200

    fixing 2-strchr.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,14 +9,20 @@
  * @b: constant byte
  * @n: the number of byte that you want to change
  *
- * Return: Always 0 (Success)
+ * Return: a pointer to the memory area s
  */
 
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	memset(s, b, sizeof(char) * n);
+	while (*s != '\0' && n > 0)
+	{
+		*s = b;
+		s++;
+		n--;
+	}
 
 	return (s);
 }
+
 
