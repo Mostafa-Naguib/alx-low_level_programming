e9c0cada0b63ec26ef5917dc757aeaf1c4e8055e
mostafa <mostafanaguib200@gmail.com>
2023-11-18 00:25:52 +0200

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * _strpbrk - A function that searches a string for any of a set of bytes.
+ * _strstr - A function that locates a substring.
  *
- * Description: A function that searches a string for any of a set of bytes.
+ * Description: A function that locates a substring.
  *
- * @s: The string that you want to search in it...
- * @accept: The pattern that you want to search for it...
+ * @haystack: The string that you want to search in it...
+ * @needle: The substring that we want to search for it...
  *
- * Return: substring fo corresponding pattern.
+ * Return: A pointer to the beginning of the located substring
  */
 
 
 char *_strstr(char *haystack, char *needle)
 {
-    if (*needle == '\0')
-        return (haystack);
+	if (*needle == '\0')
+		return (haystack);
 
-    while (*haystack != '\0')
+	while (*haystack != '\0')
 	{
-        char *h = haystack;
-        char *n = needle;
+		char *h = haystack;
+		char *n = needle;
 
-        while (*n != '\0' && *h == *n)
+		while (*n != '\0' && *h == *n)
 		{
-            h++;
-            n++;
-        }
+			h++;
+			n++;
+		}
 
-        if (*n == '\0')
-            return (haystack);
+		if (*n == '\0')
+			return (haystack);
 
-        haystack++;
-    }
+		haystack++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
