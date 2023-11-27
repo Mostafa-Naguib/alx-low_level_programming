508c14c4e21e2c227300abe974e67c65912bb5c0
mostafa <mostafanaguib200@gmail.com>
2023-11-27 17:26:31 +0200

    solving task four

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,16 @@
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
+/**
+ * new_dog - a function that creates a new dog.
+ *
+ * Description: a function that creates a new dog.
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Return: A new dog
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
