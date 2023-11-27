45753f035d0aeeb1b60330d6d2f486094cc887d1
mostafa <mostafanaguib200@gmail.com>
2023-11-27 16:35:34 +0200

    fixing a bug

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - prints the name of the file it was compiled from...
  *
@@ -10,7 +11,7 @@
 
 int main(void)
 {
-	printf("%s\n", __name__);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
