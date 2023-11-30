bace1fb02a4963c7f5ea9910c495f6e21d4f701c
mostafa <mostafanaguib200@gmail.com>
2023-11-30 23:20:04 +0200

    task two

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,43 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - a function that prints numbers, followed by a new line.
+ *
+ * Description: a function that prints numbers, followed by a new line.
+ * @n: is a fixed argument representing the number of variable arguments,
+ * @separator: is a separator between two numbers
+ *
+ */
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ptr;
+	char *s;
+
+	va_start(ptr, n);
+
+
+	if (!separator)
+		separator = "";
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(ptr, char *);
+
+		(s) ? printf("%s", s) : printf("(nil)");
+
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ptr);
+}
