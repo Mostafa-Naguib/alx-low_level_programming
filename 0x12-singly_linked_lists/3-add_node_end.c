bddd28bdde4b14d101044c79e6db1facefa28ed4
mostafa <mostafanaguib200@gmail.com>
2023-12-07 20:05:30 +0200

    fixing a bug in task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,7 +25,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	len = strlen(str);
 
 	new_node = malloc(sizeof(list_t));
-	if (!new_node || !*head)
+	if (!new_node || !head)
 		return (0);
 
 	new_node->str = strdup(str);
