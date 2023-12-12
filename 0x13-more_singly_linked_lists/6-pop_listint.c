4a9dfb252af6c4144b568f98007641e833e7558c
mostafa <mostafanaguib200@gmail.com>
2023-12-12 06:53:05 +0200

    testing

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -4,7 +4,14 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *node;
+	int n;
 
-	if (!*head)
+	if (!*head | !head)
 		return (0);
+	
+	node = *(head)->next;
+	n = *(head)->n;
+	free(*head);
+	*head = node;
+	return (n);
 }
