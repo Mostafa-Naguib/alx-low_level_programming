4f7ab0fbfe256890da17047142f8321cae4dc50a
mostafa <mostafanaguib200@gmail.com>
2023-12-12 21:57:29 +0200

    testing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(listint_t));
 
-	if (!head || !new_node || !*head)
+	if (!head || !new_node)
 		return (0);
 
 	new_node->n = n;
