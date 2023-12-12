07e4bdb7048d8223f00d3eea01c4d00c2fcf8e58
mostafa <mostafanaguib200@gmail.com>
2023-12-12 22:06:07 +0200

    tesing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,9 +17,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	unsigned int i = 0;
 
+	if (!head)
+		return (0);
+
 	new_node = malloc(sizeof(listint_t));
 
-	if (!head || !new_node)
+	if (!new_node)
 		return (0);
 
 	new_node->n = n;
