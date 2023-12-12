fea4ebf57561ba49d2525e6b7bfbcf634b27593a
mostafa <mostafanaguib200@gmail.com>
2023-12-12 07:11:55 +0200

    task seven

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,3 +2,15 @@
 
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+    listint_t *tmp;
+    unsigned int i = 0;
+
+    if (!head)
+        return (0);
+    
+    for (tmp = head; tmp && i < index; tmp = tmp->next, i++)
+        ;
+    
+    return (tmp);
+}
