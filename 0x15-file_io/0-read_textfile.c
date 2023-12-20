81bd5e2a9d4eb8f1ec4b9554d8788413b1fd450f
mostafa <mostafanaguib200@gmail.com>
2023-12-20 10:22:32 +0200

    test" ;

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,13 +18,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!filename || !letters)
 		return (0);
 
-	fd = open(filename, 0);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	byte_read = read(fd, &buffer[0], letters);
+	byte_read = read(fd, buffer, letters);
 
-	byte_read = write(1, &buffer[0], byte_read);
+	byte_read = write(STDOUT_FILENO, buffer, byte_read);
 
 	close(fd);
 	return (byte_read);
