7662d0afd4b16c52a2b2e29aa1717caa12385a32
mostafa <mostafanaguib200@gmail.com>
2023-12-21 06:10:15 +0200

    testing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int _strlen(char *s);
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t len = strlen(text_content);
+	size_t len = _strlen(text_content);
 	size_t byte_read;
 
 	if (!filename)
