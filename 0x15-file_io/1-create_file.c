5d5bd0740fc1cfe89c5a03e310d647f6a33fb95f
mostafa <mostafanaguib200@gmail.com>
2023-12-21 07:53:25 +0200

    testing

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,3 +37,24 @@ int create_file(const char *filename, char *text_content)
 }
 
 
+/**
+ * _strlen - A function that returns the length of a string..
+ *
+ * Description: A function that returns the length of a string.
+ * @s: the string that you want to count...
+ *
+ * Return: The length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	for (; s[i] != '\0'; i++)
+		;
+
+	return (i);
+}
