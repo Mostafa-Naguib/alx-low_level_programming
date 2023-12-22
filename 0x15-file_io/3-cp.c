026ff275dac9c652a9351516b15fb93566b3ee87
mostafa <mostafanaguib200@gmail.com>
2023-12-22 11:33:28 +0200

    fixing some bugs

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,42 +9,38 @@
  * @argc: The number of argument.
  * @argv: The array of argument.
  *
+ * Return: EXIT_SUCCESS on success...
  */
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int fd_src, fd_dest;
 	char *buffer[BUFFER_SIZE];
-	size_t bytes_wrote, bytes_read;
+	ssize_t bytes_wrote, bytes_read;
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	}
+
 
 	fd_src = open(argv[1], O_RDWR);
 	if (fd_src == -1)
-	{
 		dprintf(STDERR_FILENO, ERR_SRC, argv[1]), exit(98);
-	}
+
 
 	fd_dest = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fd_dest == -1)
-	{
 		dprintf(STDERR_FILENO, ERR_DEST, argv[2]), exit(99);
-	}
+
 
 	bytes_read = read(fd_src, buffer, BUFFER_SIZE);
 	if (bytes_read == -1)
-	{
 		dprintf(STDERR_FILENO, ERR_SRC, argv[1]), exit(98);
-	}
+
 
 	bytes_wrote = write(fd_dest, buffer, bytes_read);
 	if (bytes_wrote != bytes_read)
-	{
 		dprintf(STDERR_FILENO, ERR_DEST, argv[2]), exit(99);
-	}
+
 
 	fd_src = close(fd_src);
 	if (fd_src)
@@ -53,4 +49,6 @@ void main(int argc, char *argv[])
 	fd_dest = close(fd_dest);
 	if (fd_dest)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd_src), exit(100);
+
+	exit(EXIT_SUCCESS);
 }
